5ebef4dea1449752a9789fe4fc30138518122448
CareerCloudHub2024 <careercloudhub@gmail.com>
2025-02-10 12:43:14 +0530

    Functionality Update

--- data.js
+++ data.js
@@ -2,199 +2,164 @@ const gifts = [
     {
       "name": "Layered Clover Pendant Necklace",
       "price": "999",
+      "category":"Necklace",
       "link": "https://shopdew.com/product/layered-clover-pendant-necklace/",
       "index": 0
     },
     {
       "name": "Classic Diamond Pendant Necklace",
       "price": "499",
+      "category":"Necklace",
       "link": "https://shopdew.com/product/classic-diamond-pendant-necklace/",
       "index": 1
     },
     {
-      "name": "Adjustable Mirror Ring and Trinket Dish Set",
+      "name": "Blue Crystal Evil Eye Necklace with Gold-Tone Detailing",
       "price": "799",
-      "link": "https://shopdew.com/product/adjustable-mirror-ring-and-trinket-dish-set/",
+      "category":"Necklace",
+      "link": "https://shopdew.com/product/blue-crystal-evil-eye-necklace-with-gold-tone-detailing/",
       "index": 2
-    },
-    {
-      "name": "Twisted Gold Hoop Earrings",
-      "price": "639",
-      "link": "https://shopdew.com/product/twisted-gold-hoop-earrings/",
-      "index": 3
-    },
-    {
-      "name": "Unique Gold-Tone Hoop Earrings",
-      "price": "719",
-      "link": "https://shopdew.com/product/unique-gold-tone-hoop-earrings/",
-      "index": 4
     },
     {
       "name": "Butterfly Chain Necklace",
       "price": "799",
+      "category":"Necklace",
       "link": "https://shopdew.com/product/butterfly-chain-necklace/",
+      "index": 3
+    },
+    {
+      "name": "Crystal Blue Evil Eye Pendent Necklace",
+      "price": "799",
+      "category":"Necklace",
+      "link": "https://shopdew.com/product/crystal-blue-evil-eye-pendent-necklace/",
+      "index": 4
+    },
+    {
+      "name": "Elegant Pearl and Beaded Necklace",
+      "price": "799",
+      "category":"Necklace",
+      "link": "https://shopdew.com/product/elegant-pearl-and-beaded-necklace-set/",
       "index": 5
     },
     {
-      "name": "Sunburst Statement Earrings",
-      "price": "510",
-      "link": "https://shopdew.com/product/sunburst-statement-earrings/",
+      "name": "Elegant Pearl and Kundan Necklace",
+      "price": "550",
+      "category":"Necklace",
+      "link": "https://shopdew.com/product/elegant-pearl-and-kundan-necklace/",
       "index": 6
     },
     {
-      "name": "Blue Crystal Evil Eye Necklace with Gold-Tone Detailing",
-      "price": "799",
-      "link": "https://shopdew.com/product/blue-crystal-evil-eye-necklace-with-gold-tone-detailing/",
+      "name": "Evil Eye Pendant Necklace",
+      "price": "699",
+      "category":"Necklace",
+      "link": "https://shopdew.com/product/evil-eye-pendant-necklace/",
       "index": 7
     },
     {
       "name": "Evil Eye Protection Necklace with Colorful Crystals",
       "price": "510",
+      "category":"Necklace",
       "link": "https://shopdew.com/product/evil-eye-protection-necklace-with-colorful-crystals/",
       "index": 8
     },
     {
+      "name": "Star-Studded Pendant Necklace",
+      "price": "639",
+      "category":"Necklace",
+      "link": "https://shopdew.com/product/star-studded-pendant-necklace/",
+      "index": 9
+    },
+
+    //Earings
+    {
+      "name": "Twisted Gold Hoop Earrings",
+      "price": "639",
+      "category":"Earings",
+      "link": "https://shopdew.com/product/twisted-gold-hoop-earrings/",
+      "index": 10
+    },
+    {
+      "name": "Unique Gold-Tone Hoop Earrings",
+      "price": "719",
+      "category":"Earings",
+      "link": "https://shopdew.com/product/unique-gold-tone-hoop-earrings/",
+      "index": 11
+    },
+    {
+      "name": "Sunburst Statement Earrings",
+      "price": "510",
+      "category":"Earings",
+      "link": "https://shopdew.com/product/sunburst-statement-earrings/",
+      "index": 12
+    },
+    {
       "name": "Vintage Rope-Twist Earrings",
       "price": "799",
+      "category":"Earings",
       "link": "https://shopdew.com/product/vintage-rope-twist-earrings/",
-      "index": 9
+      "index": 13
     },
     {
       "name": "Vibrant Woven Earrings",
       "price": "450",
+      "category":"Earings",
       "link": "https://shopdew.com/product/vibrant-woven-earrings/",
-      "index": 10
+      "index": 14
     },
     {
-      "name": "Evil Eye Protection Necklace with Sparkling Crystals",
-      "price": "699",
-      "link": "https://shopdew.com/product/evil-eye-protection-necklace-with-sparkling-crystals/",
-      "index": 11
+      "name": "11-Pair Gold-Tone Earrings Set: From Studs to Statement Pieces",
+      "price": "560",
+      "category":"Earings",
+      "link": "https://shopdew.com/product/11-pair-gold-tone-earrings-set-from-studs-to-statement-pieces/",
+      "index": 15
+    },
+    {
+      "name": "Celestial Star Earrings",
+      "price": "490",
+      "category":"Earings",
+      "link": "https://shopdew.com/product/celestial-star-earrings/",
+      "index": 16
+    },
+    {
+      "name": "Elegant Jhumka Earrings",
+      "price": "525",
+      "category":"Earings",
+      "link": "https://shopdew.com/product/elegant-jhumka-earrings/",
+      "index": 17
+    },
+    {
+      "name": "Elegant Seashell Hoop Earrings",
+      "price": "450",
+      "category":"Earings",
+      "link": "https://shopdew.com/product/elegant-seashell-hoop-earrings/",
+      "index": 18
+    },
+    {
+      "name": "Modern Geometric Dangle Earrings with Pearl Accent",
+      "price": "480",
+      "category":"Earings",
+      "link": "https://shopdew.com/product/modern-geometric-dangle-earrings-with-pearl-accent/",
+      "index": 19
+    },
+    {
+      "name": "Adjustable Mirror Ring and Trinket Dish Set",
+      "price": "799",
+      "category":"Ring",
+      "link": "https://shopdew.com/product/adjustable-mirror-ring-and-trinket-dish-set/",
+      "index": 20
     },
     {
       "name": "Sparkling Crystal Tiara",
       "price": "1200",
+      "category":"Ring",
       "link": "https://shopdew.com/product/sparkling-crystal-tiara/",
-      "index": 12
-    },
-    {
-      "name": "Elegant Pearl Drop Earrings",
-      "price": "550",
-      "link": "https://shopdew.com/product/elegant-pearl-drop-earrings/",
-      "index": 13
-    },
-    {
-      "name": "Charming Star Pendant Necklace",
-      "price": "650",
-      "link": "https://shopdew.com/product/charming-star-pendant-necklace/",
-      "index": 14
-    },
-    {
-      "name": "Dainty Gold Anklet",
-      "price": "400",
-      "link": "https://shopdew.com/product/dainty-gold-anklet/",
-      "index": 15
-    },
-    {
-      "name": "Floral Hair Clip Set",
-      "price": "300",
-      "link": "https://shopdew.com/product/floral-hair-clip-set/",
-      "index": 16
-    },
-    {
-      "name": "Geometric Statement Ring",
-      "price": "700",
-      "link": "https://shopdew.com/product/geometric-statement-ring/",
-      "index": 17
-    },
-    {
-      "name": "Minimalist Bar Necklace",
-      "price": "600",
-      "link": "https://shopdew.com/product/minimalist-bar-necklace/",
-      "index": 18
-    },
-    {
-      "name": "Boho Beaded Bracelet",
-      "price": "450",
-      "link": "https://shopdew.com/product/boho-beaded-bracelet/",
-      "index": 19
-    },
-    {
-      "name": "Crystal Drop Earrings",
-      "price": "500",
-      "link": "https://shopdew.com/product/crystal-drop-earrings/",
-      "index": 20
-    },
-    {
-      "name": "Leaf Pendant Necklace",
-      "price": "550",
-      "link": "https://shopdew.com/product/leaf-pendant-necklace/",
       "index": 21
     },
     {
-      "name": "Pearl Stud Earrings",
-      "price": "400",
-      "link": "https://shopdew.com/product/pearl-stud-earrings/",
+      "name": "Clear Quartz Evil Eye Bracelet",
+      "price": "639",
+      "category":"Ring",
+      "link": "https://shopdew.com/product/clear-quartz-evil-eye-bracelet/",
       "index": 22
     },
-    {
-      "name": "Adjustable Knot Bracelet",
-      "price": "350",
-      "link": "https://shopdew.com/product/adjustable-knot-bracelet/",
-      "index": 23
-    },
-    {
-      "name": "Statement Tassel Earrings",
-      "price": "600",
-      "link": "https://shopdew.com/product/statement-tassel-earrings/",
-      "index": 24
-    },
-    {
-      "name": "Infinity Pendant Necklace",
-      "price": "700",
-      "link": "https://shopdew.com/product/infinity-pendant-necklace/",
-      "index": 25
-    },
-    {
-      "name": "Heart Charm Bracelet",
-      "price": "500",
-      "link": "https://shopdew.com/product/heart-charm-bracelet/",
-      "index": 26
-    },
-    {
-      "name": "Feather Drop Earrings",
-      "price": "550",
-      "link": "https://shopdew.com/product/feather-drop-earrings/",
-      "index": 27
-    },
-    {
-      "name": "Crescent Moon Necklace",
-      "price": "650",
-      "link": "https://shopdew.com/product/crescent-moon-necklace/",
-      "index": 28
-    },
-    {
-      "name": "Beaded Hoop Earrings",
-      "price": "450",
-      "link": "https://shopdew.com/product/beaded-hoop-earrings/",
-      "index": 29
-    },
-    {
-      "name": "Charm Anklet Set",
-      "price": "500",
-      "link": "https://shopdew.com/product/charm-anklet-set/",
-      "index": 30
-    },
-    {
-      "name": "Geometric Drop Earrings",
-      "price": "600",
-      "link": "https://shopdew.com/product/geometric-drop-earrings/",
-      "index": 31
-    },
-    {
-      "name": "Stackable Ring Set",
-      "price": "750",
-      "link": "https://shopdew.com/product/stackable-ring-set/",
-      "index": 32
-    }
   ]  
--- script.js
+++ script.js
@@ -1,204 +1,170 @@
 const gifts = [
+  //Necklace
   {
     "name": "Layered Clover Pendant Necklace",
     "price": "999",
+    "category":"Necklace",
     "link": "https://shopdew.com/product/layered-clover-pendant-necklace/",
     "index": 0
   },
   {
     "name": "Classic Diamond Pendant Necklace",
     "price": "499",
+    "category":"Necklace",
     "link": "https://shopdew.com/product/classic-diamond-pendant-necklace/",
     "index": 1
   },
   {
-    "name": "Adjustable Mirror Ring and Trinket Dish Set",
+    "name": "Blue Crystal Evil Eye Necklace with Gold-Tone Detailing",
     "price": "799",
-    "link": "https://shopdew.com/product/adjustable-mirror-ring-and-trinket-dish-set/",
+    "category":"Necklace",
+    "link": "https://shopdew.com/product/blue-crystal-evil-eye-necklace-with-gold-tone-detailing/",
     "index": 2
-  },
-  {
-    "name": "Twisted Gold Hoop Earrings",
-    "price": "639",
-    "link": "https://shopdew.com/product/twisted-gold-hoop-earrings/",
-    "index": 3
-  },
-  {
-    "name": "Unique Gold-Tone Hoop Earrings",
-    "price": "719",
-    "link": "https://shopdew.com/product/unique-gold-tone-hoop-earrings/",
-    "index": 4
   },
   {
     "name": "Butterfly Chain Necklace",
     "price": "799",
+    "category":"Necklace",
     "link": "https://shopdew.com/product/butterfly-chain-necklace/",
+    "index": 3
+  },
+  // {
+  //   "name": "Crystal Blue Evil Eye Pendent Necklace",
+  //   "price": "799",
+  //   "category":"Necklace",
+  //   "link": "https://shopdew.com/product/crystal-blue-evil-eye-pendent-necklace/",
+  //   "index": 4
+  // },
+  {
+    "name": "Elegant Pearl and Beaded Necklace",
+    "price": "799",
+    "category":"Necklace",
+    "link": "https://shopdew.com/product/elegant-pearl-and-beaded-necklace-set/",
     "index": 5
-  },
-  {
-    "name": "Sunburst Statement Earrings",
-    "price": "510",
-    "link": "https://shopdew.com/product/sunburst-statement-earrings/",
-    "index": 6
-  },
-  {
-    "name": "Blue Crystal Evil Eye Necklace with Gold-Tone Detailing",
-    "price": "799",
-    "link": "https://shopdew.com/product/blue-crystal-evil-eye-necklace-with-gold-tone-detailing/",
-    "index": 7
-  },
-  {
-    "name": "Evil Eye Protection Necklace with Colorful Crystals",
-    "price": "510",
-    "link": "https://shopdew.com/product/evil-eye-protection-necklace-with-colorful-crystals/",
-    "index": 8
-  },
-  {
-    "name": "Vintage Rope-Twist Earrings",
-    "price": "799",
-    "link": "https://shopdew.com/product/vintage-rope-twist-earrings/",
-    "index": 9
-  },
-  {
-    "name": "Vibrant Woven Earrings",
-    "price": "450",
-    "link": "https://shopdew.com/product/vibrant-woven-earrings/",
-    "index": 10
-  },
-  {
-    "name": "Evil Eye Protection Necklace with Sparkling Crystals",
-    "price": "699",
-    "link": "https://shopdew.com/product/evil-eye-protection-necklace-with-sparkling-crystals/",
-    "index": 11
-  },
-  {
-    "name": "Sparkling Crystal Tiara",
-    "price": "1200",
-    "link": "https://shopdew.com/product/sparkling-crystal-tiara/",
-    "index": 12
-  },
-  {
-    "name": "Elegant Pearl Drop Earrings",
-    "price": "550",
-    "link": "https://shopdew.com/product/elegant-pearl-drop-earrings/",
-    "index": 13
-  },
-  {
-    "name": "Charming Star Pendant Necklace",
-    "price": "650",
-    "link": "https://shopdew.com/product/charming-star-pendant-necklace/",
-    "index": 14
-  },
-  {
-    "name": "Dainty Gold Anklet",
-    "price": "400",
-    "link": "https://shopdew.com/product/dainty-gold-anklet/",
-    "index": 15
-  },
-  {
-    "name": "Floral Hair Clip Set",
-    "price": "300",
-    "link": "https://shopdew.com/product/floral-hair-clip-set/",
-    "index": 16
-  },
-  {
-    "name": "Geometric Statement Ring",
-    "price": "700",
-    "link": "https://shopdew.com/product/geometric-statement-ring/",
-    "index": 17
-  },
-  {
-    "name": "Minimalist Bar Necklace",
-    "price": "600",
-    "link": "https://shopdew.com/product/minimalist-bar-necklace/",
-    "index": 18
-  },
-  {
-    "name": "Boho Beaded Bracelet",
-    "price": "450",
-    "link": "https://shopdew.com/product/boho-beaded-bracelet/",
-    "index": 19
-  },
-  {
-    "name": "Yellow Chimes Women’s Butterfly Designed Dangle Drop Earrings",
-    "price": "899",
-    "link": "https://shopdew.com/product/yellow-chimes-womens-butterfly-designed-dangle-drop-earrings/",
-    "index": 20
   },
   {
     "name": "Elegant Pearl and Kundan Necklace",
     "price": "550",
+    "category":"Necklace",
     "link": "https://shopdew.com/product/elegant-pearl-and-kundan-necklace/",
-    "index": 21
+    "index": 6
   },
+  // {
+  //   "name": "Evil Eye Pendant Necklace",
+  //   "price": "699",
+  //   "category":"Necklace",
+  //   "link": "https://shopdew.com/product/evil-eye-pendant-necklace/",
+  //   "index": 7
+  // },
+  {
+    "name": "Evil Eye Protection Necklace with Colorful Crystals",
+    "price": "510",
+    "category":"Necklace",
+    "link": "https://shopdew.com/product/evil-eye-protection-necklace-with-colorful-crystals/",
+    "index": 8
+  },
+  {
+    "name": "Star-Studded Pendant Necklace",
+    "price": "639",
+    "category":"Necklace",
+    "link": "https://shopdew.com/product/star-studded-pendant-necklace/",
+    "index": 9
+  },
+
+  //Earings
+  {
+    "name": "Twisted Gold Hoop Earrings",
+    "price": "639",
+    "category":"Earings",
+    "link": "https://shopdew.com/product/twisted-gold-hoop-earrings/",
+    "index": 10
+  },
+  {
+    "name": "Unique Gold-Tone Hoop Earrings",
+    "price": "719",
+    "category":"Earings",
+    "link": "https://shopdew.com/product/unique-gold-tone-hoop-earrings/",
+    "index": 11
+  },
+  {
+    "name": "Sunburst Statement Earrings",
+    "price": "510",
+    "category":"Earings",
+    "link": "https://shopdew.com/product/sunburst-statement-earrings/",
+    "index": 12
+  },
+  {
+    "name": "Vintage Rope-Twist Earrings",
+    "price": "799",
+    "category":"Earings",
+    "link": "https://shopdew.com/product/vintage-rope-twist-earrings/",
+    "index": 13
+  },
+  // {
+  //   "name": "Vibrant Woven Earrings",
+  //   "price": "450",
+  //   "category":"Earings",
+  //   "link": "https://shopdew.com/product/vibrant-woven-earrings/",
+  //   "index": 14
+  // },
   {
     "name": "11-Pair Gold-Tone Earrings Set: From Studs to Statement Pieces",
     "price": "560",
+    "category":"Earings",
     "link": "https://shopdew.com/product/11-pair-gold-tone-earrings-set-from-studs-to-statement-pieces/",
-    "index": 22
+    "index": 15
   },
   {
-    "name": "Traditional Conch Shell Anklet",
-    "price": "450",
-    "link": "https://shopdew.com/product/traditional-conch-shell-anklet/",
-    "index": 23
+    "name": "Celestial Star Earrings",
+    "price": "490",
+    "category":"Earings",
+    "link": "https://shopdew.com/product/celestial-star-earrings/",
+    "index": 16
   },
   {
-    "name": "Elegant Tassel Drop Earrings",
-    "price": "899",
-    "link": "https://shopdew.com/product/elegant-tassel-drop-earrings/",
-    "index": 24
-  },
-  {
-    "name": "Treble Clef Crystal Pendant Necklace",
-    "price": "799",
-    "link": "https://shopdew.com/product/treble-clef-crystal-pendant-necklace/",
-    "index": 25
-  },
-  {
-    "name": "Sparkling Crystal Tiara",
-    "price": "639",
-    "link": "https://shopdew.com/product-category/sparkling-crystal-tiara/",
-    "index": 26
-  },
-  {
-    "name": "Floral Elegance Long Drop Earrings",
-    "price": "499",
-    "link": "https://shopdew.com/product/floral-elegance-long-drop-earrings/",
-    "index": 27
-  },
-  {
-    "name": "Evil Eye Gold-Plated Stone-Studded Jewelry Set",
-    "price": "799",
-    "link": "https://shopdew.com/product/evil-eye-gold-plated-stone-studded-jewelry-set/",
-    "index": 28
+    "name": "Elegant Jhumka Earrings",
+    "price": "525",
+    "category":"Earings",
+    "link": "https://shopdew.com/product/elegant-jhumka-earrings/",
+    "index": 17
   },
   {
     "name": "Elegant Seashell Hoop Earrings",
     "price": "450",
+    "category":"Earings",
     "link": "https://shopdew.com/product/elegant-seashell-hoop-earrings/",
-    "index": 29
+    "index": 18
   },
   {
-    "name": "Colorful Seashell Bracelets",
-    "price": "450",
-    "link": "https://shopdew.com/product/colorful-seashell-bracelets/",
-    "index": 30
+    "name": "Modern Geometric Dangle Earrings with Pearl Accent",
+    "price": "480",
+    "category":"Earings",
+    "link": "https://shopdew.com/product/modern-geometric-dangle-earrings-with-pearl-accent/",
+    "index": 19
   },
+  //Rings
   {
-    "name": "Yellow Chimes Women’s Butterfly Designed Dangle Drop Earrings",
-    "price": "899",
-    "link": "https://shopdew.com/product/yellow-chimes-womens-butterfly-designed-dangle-drop-earrings/",
-    "index": 31
-  },
-  {
-    "name": "Shining Diva Fashion Latest Stylish Set of 4 Finger Rings",
+    "name": "Adjustable Mirror Ring and Trinket Dish Set",
     "price": "799",
-    "link": "https://shopdew.com/product/shining-diva-fashion-latest-stylish-set-of-4-finger-rings/",
-    "index": 32
-  }
-]  
-
+    "category":"Ring",
+    "link": "https://shopdew.com/product/adjustable-mirror-ring-and-trinket-dish-set/",
+    "index": 20
+  },
+  {
+    "name": "Sparkling Crystal Tiara",
+    "price": "1200",
+    "category":"Ring",
+    "link": "https://shopdew.com/product/sparkling-crystal-tiara/",
+    "index": 21
+  },
+  {
+    "name": "Clear Quartz Evil Eye Bracelet",
+    "price": "639",
+    "category":"Ring",
+    "link": "https://shopdew.com/product/clear-quartz-evil-eye-bracelet/",
+    "index": 22
+  },
+]
   function predictGift() {
     const partnerName = document.querySelector('#partner-name').value.trim();
     const partnerAge = parseInt(document.querySelector('#partner-age').value);
@@ -214,22 +180,25 @@ const gifts = [
   
     // Filter gifts by budget
     let availableGifts = gifts.filter(gift => {
-      if (budget === 'low' && gift.price <= 1000) return true;
-      if (budget === 'medium' && gift.price > 1000 && gift.price <= 2000) return true;
-      if (budget === 'high' && gift.price > 2000) return true;
+      if (budget === 'low' && gift?.price <= 1000 && gift?.category == interests) return true;
+      if (budget === 'medium' && gift?.price > 1000 && gift.price <= 2000 && gift?.category == interests) return true;
+      if (budget === 'high' && gift?.price > 2000 && gift?.category == interests) return true;
       return false;
     });
   
     // If no gifts in budget, use all gifts
     if (availableGifts.length === 0) {
-      availableGifts = gifts;
+      availableGifts = gifts.filter(gift => {
+        if (gift?.category == interests) return true;
+        return false;
+      });
     }
   
     const getRandomIndex = (min,max)=>{
       return Math.floor(Math.random() * (max - min) + min)
     }
 
-    console.log(getRandomIndex(0,availableGifts.length))
+    // console.log(getRandomIndex(0,availableGifts.length))
     // Prediction logic based on interests and relationship
     let selectedGift = availableGifts[getRandomIndex(0,availableGifts.length)];
     
@@ -259,3 +228,4 @@ const gifts = [
   }
   
   document.querySelector('#predict-btn').addEventListener('click', predictGift);
+  document.querySelector('#discover-btn').addEventListener('click', predictGift);
